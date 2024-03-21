6a44a3f4310b91b9f7ce48c449b1a9fe36cdb13f
mazaherireza <rezamazaheri.email@gmail.com>
2024-03-21 07:33:31 +0330

    Elementary: Nan, isNaN

---
+++ Elementary/4- isNaN() Method/project/js/index.js
@@ -0,0 +1,7 @@
+let programmingLanguage = "JavaScript";
+let version = 12;
+
+console.log(`${programmingLanguage} ${version}`);
+
+console.log(isNaN(programmingLanguage + version)); // true
+console.log(isNaN(13 / 4)); // false
