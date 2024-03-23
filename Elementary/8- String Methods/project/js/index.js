d7b32fd67f4d1fefee9b0ff9346479bdcda3f05d
mazaherireza <rezamazaheri.email@gmail.com>
2024-03-23 10:23:51 +0330

    Elementary: String Methods

---
+++ Elementary/8- String Methods/project/js/index.js
@@ -0,0 +1,8 @@
+const motto = 'I Love JavaScript'
+
+console.log(motto.charCodeAt(0)) // ASCII Code
+
+console.log(motto.concat(' & VueJS'))
+
+console.log(motto.search('Java'))
+console.log(motto.search('java'))
