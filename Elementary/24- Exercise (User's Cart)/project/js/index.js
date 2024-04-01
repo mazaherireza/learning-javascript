89a9d37269d174a9e3b40c72d1a3154c533bf5fa
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-01 09:41:37 +0330

    Elementary: Exercise (User's Cart)

---
+++ Elementary/24- Exercise (User's Cart)/project/js/index.js
@@ -0,0 +1,50 @@
+const availableBooks = [
+  {
+    id: "BAR_1001_0",
+    title: "1984",
+    author: "George Orwell",
+    price: 13,
+  },
+  {
+    id: "BAR_1001_1",
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel García Márquez",
+    price: 25,
+  },
+];
+const ID = "CRD_1001_";
+const selectedBook = prompt(
+  "Enter Book's Title That You Want To Buy:",
+  "Book's Title"
+);
+let temp = {};
+const found = availableBooks.some((book) => {
+  if (book.title == selectedBook) {
+    const { id, ...other } = book;
+    temp = other;
+    return true;
+  }
+});
+
+const cart = [];
+
+const calculateSum = () => {
+  let sum = 0;
+  cart.forEach((item) => {
+    sum += item.price;
+  });
+  return sum;
+};
+
+if (found) {
+  const index = cart.length;
+  cart.push({
+    id: `${ID}${index}`,
+    ...temp,
+  });
+
+  console.log("Your Cart Is: ", cart);
+  console.log("Total Price Is:", calculateSum());
+} else {
+  console.log("Not Found.");
+}
