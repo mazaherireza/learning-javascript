4506eff5d0dbcc10b69777a141b4674191f9f7d9
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-09 13:31:00 +0330

    Elementary: Exercise (Simple Calculator)

---
+++ Elementary/35- Exercise (Simple Calculator)/project/js/index.js
@@ -0,0 +1,30 @@
+const num1 = +prompt("Enter first number: ");
+const num2 = +prompt("Enter second number: ");
+
+const operator = prompt(`Enter the operator:
+  1. +
+  2. -
+  3. *
+  4. /
+  5. **
+`);
+
+switch (operator) {
+  case "1":
+    console.log(`Result is: ${num1 + num2}`);
+    break;
+  case "2":
+    console.log(`Result is: ${num1 - num2}`);
+    break;
+  case "3":
+    console.log(`Result is: ${num1 * num2}`);
+    break;
+  case "4":
+    console.log(`Result is: ${num1 / num2}`);
+    break;
+  case "5":
+    console.log(`Result is: ${num1 ** num2}`);
+    break;
+  default:
+    console.log("Invalid operator!");
+}
