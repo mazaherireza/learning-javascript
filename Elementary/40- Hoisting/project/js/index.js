b3bee47c80850d9d7145abd0b506c25c7a3d3160
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-12 12:22:28 +0330

    Elementary: Hoisting

---
+++ Elementary/40- Hoisting/project/js/index.js
@@ -0,0 +1,14 @@
+// Hoisting
+
+x = 1;
+console.log(`x is: ${x}`); // x is: 1
+var x;
+
+console.log(`y is ${y}`); // y is: undefined
+var y = 11;
+
+test();
+
+function test() {
+  console.log("Test is called.");
+}
