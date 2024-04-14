f59c1649a7bb1797ce8c670be9da7225e4e12846
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-14 12:38:18 +0330

    Intermediate: Concept of DOM

---
+++ Intermediate/1- Concept of DOM/project/js/index.js
@@ -0,0 +1,4 @@
+const heroTitle = document.getElementById("heroTitle");
+
+console.log(`heroTitle is: ${heroTitle}`); // heroTitle is: [object HTMLHeadingElement]
+console.log(heroTitle); // <h1 id="heroTitle">Learning JavaScript.</h1>
