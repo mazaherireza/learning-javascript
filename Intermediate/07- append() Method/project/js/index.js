262a61920bddf5c40e0bbb132609aa57bd98f74c
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-18 17:46:17 +0330

    Intermediate: append() Method

---
+++ Intermediate/07- append() Method/project/js/index.js
@@ -0,0 +1,7 @@
+const unorderedList = document.querySelector("ul");
+
+const liElement = document.createElement("li");
+liElement.innerHTML = "4th";
+unorderedList.append(liElement);
+// Or
+// unorderedList.appendChild(liElement)
