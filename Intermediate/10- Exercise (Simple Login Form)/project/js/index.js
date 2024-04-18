410d15942ea6c03782eccb09261366ba73453fdc
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-18 10:12:06 +0330

    Intermediate: Exercise (Simple Login Form)

---
+++ Intermediate/10- Exercise (Simple Login Form)/project/js/index.js
@@ -0,0 +1,29 @@
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+
+const isValidUsername = (username) => {
+  return username.length >= 10 ? true : false;
+};
+
+const isValidPassword = (password) => {
+  return password.length >= 8 ? true : false;
+};
+
+const validate = (event) => {
+  event.preventDefault();
+  let username = usernameInput.value;
+  let password = passwordInput.value;
+
+  const modal = document.querySelector(".modal");
+  modal.style.visibility = "visible";
+  if (isValidUsername(username) && isValidPassword(password)) {
+    modal.innerHTML = "Welcome";
+    modal.style.background = "linear-gradient(to left, #78ffd6, #a8ff78)";
+  } else {
+    modal.innerHTML = "Invalid!, Try Again.";
+    modal.style.background = "linear-gradient(to left, #7C0A02, #fd5c63)";
+  }
+  setTimeout(() => {
+    modal.style.visibility = "hidden";
+  }, 5000);
+};
