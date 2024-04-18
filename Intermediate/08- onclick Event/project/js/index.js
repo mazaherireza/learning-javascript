5e027c0ca1bdcdd26aa8ae6d4f29514520442023
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-18 17:56:14 +0330

    Intermediate: onclick Event

---
+++ Intermediate/08- onclick Event/project/js/index.js
@@ -0,0 +1,8 @@
+const applyStyle = () => {
+  const title = document.querySelector("#playground-title");
+  title.style.color = "#78ffd6";
+  title.style.background = "#000";
+  title.style.padding = "0.5em";
+  title.style.borderRadius = "0.25em";
+  title.style.fontWeight = "bold";
+};
