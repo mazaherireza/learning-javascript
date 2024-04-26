d58be7975bcf02aa8b5473933e77a5b8c146283a
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-26 16:11:11 +0330

    Intermediate: Custome Context Menu

---
+++ Intermediate/26- Custome Context Menu/project/js/index.js
@@ -0,0 +1,23 @@
+const $ = document;
+
+const playground = $.querySelector("#playground");
+const contextMenu = $.querySelector("#context-menu");
+
+playground.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  contextMenu.style.left = event.offsetX + "px";
+  contextMenu.style.top = event.offsetY + "px";
+  if (contextMenu.style.display === "none") {
+    contextMenu.style.display = "block";
+  }
+});
+
+playground.addEventListener("click", () => {
+  contextMenu.style.display = "none";
+});
+
+$.body.addEventListener("keydown", (event) => {
+  if (event.code === "Escape") {
+    contextMenu.style.display = "none";
+  }
+});
