ca508220016b11b492a1723b16653e18c3fc4adc
mazaherireza <rezamazaheri.email@gmail.com>
2024-04-28 16:33:02 +0330

    Intermediate: onunload Event

---
+++ Intermediate/31- onunload Event/project/js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('unload', () => {
+  console.log('Unloaded!')
+})
+
+// onunloaded occurs when the user navigates away from the page (by clicking on a link, 
+// submitting a form, closing the browser window, etc.)
