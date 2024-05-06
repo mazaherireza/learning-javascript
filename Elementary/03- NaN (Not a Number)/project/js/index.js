b91d6dab60eb7dc9e11a160b7a9da066d5f11de7
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-06 18:17:19 +0330

    Elementary: NaN (Not a Number) (Some Changes)

--- Elementary/03- NaN (Not a Number)/project/js/index.js
+++ Elementary/03- NaN (Not a Number)/project/js/index.js
@@ -1,17 +1,17 @@
-let programmingLanguage = 'JavaScript'
-let version = 12
+let programmingLanguage = "JavaScript";
+let version = 12;
 
-console.log(`${programmingLanguage} ${version}`)
-console.log(programmingLanguage - version) // NaN
+console.log(`${programmingLanguage} ${version}`);
+console.log(programmingLanguage - version); // NaN
 
-console.log(typeof NaN) // number
+console.log(typeof NaN); // number
 
-console.log('13' + 3) // 133 (Concatenation)
+console.log("13" + 3); // 133 (Concatenation)
 
-console.log('10' - 3) // 7
+console.log("10" - 3); // 7
 
-console.log('10' / 2) // 5
+console.log("10" / 2); // 5
 
-console.log('12' * 3) // 36
+console.log("12" * 3); // 36
 
-console.log('2' ** 3) // 8
+console.log("2" ** 3); // 8
