6dea10e410c95dd7c7f7a3245a23c08b43dba512
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-06 16:23:51 +0330

    Elementary: Data Types (Some Changes)

--- Elementary/01- Data Types/project/js/index.js
+++ Elementary/01- Data Types/project/js/index.js
@@ -1,12 +1,12 @@
-let numberOfCourses = 18
-let nameOfCourse = 'Machine Learning'
-let isOnline = true
+let numberOfCourses = 18;
+let nameOfCourse = "Machine Learning";
+let isOnline = true;
 
-console.log('Number of Courses is: ', numberOfCourses)
-console.log('Type of numberOfCourses is: ', typeof numberOfCourses)
+console.log("Number of Courses is: ", numberOfCourses);
+console.log("Type of numberOfCourses is: ", typeof numberOfCourses);
 
-console.log('Name of Course is: ', nameOfCourse)
-console.log('Type of nameOfCourse is: ', typeof nameOfCourse)
+console.log("Name of Course is: ", nameOfCourse);
+console.log("Type of nameOfCourse is: ", typeof nameOfCourse);
 
-console.log('Are You Online?! ', isOnline ? 'Yes' : 'No!')
-console.log('Type of isOnline is: ', typeof isOnline)
+console.log("This course is", isOnline ? "online" : "offline");
+console.log("Type of isOnline is: ", typeof isOnline);
