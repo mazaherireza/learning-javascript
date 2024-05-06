95b3cba7ccbdc687664fec2b58da60abcfd90945
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-06 18:49:20 +0330

    Elementary: isNaN() Method (Some Changes)

--- Elementary/04- isNaN() Method/project/js/index.js
+++ Elementary/04- isNaN() Method/project/js/index.js
@@ -5,3 +5,5 @@ console.log(`${programmingLanguage} ${version}`);
 
 console.log(isNaN(programmingLanguage + version)); // true
 console.log(isNaN(13 / 4)); // false
+console.log(isNaN(NaN)); // true
+console.log(isNaN("")); // false <---- Converts the value to a number before testing it. Number("") becomes 0
