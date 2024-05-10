aaf3e02496af3bf8fbbe420fd609ed37ae912c9e
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-10 11:17:18 +0330

    Elementaryconst regions = [

--- Elementary/36- Exercise (IIFE)/project/js/index.js
+++ Elementary/36- Exercise (IIFE)/project/js/index.js
@@ -20,7 +20,7 @@ const employee = employees.find(
   (employee) => employee._id === "EMP_1001"
 );
 
-// IIFE (Immediately Invoked Function Expressions) Function
+// IIFE (Immediately Invoked Function Expressions)
 // A soon as function is created it invokes itself doesn’t need to invoke explicitly.
 
 (() => {
@@ -42,5 +42,4 @@ console.log(`iife is: ${iife}`);
 // https://www.freecodecamp.org/news/iife-in-javascript-what/
 
 // This ensures that code inside IIFE does not interfere with other code or be interfered by another code and so code is safe.
-
 // One way to prevent the functions and variables from polluting the global object is to use immediately invoked function expressions.
