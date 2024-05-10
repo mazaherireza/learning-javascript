d3f8e5a9d9825c8999b6d164c0d6271558c88b54
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-10 14:25:52 +0330

    Elementary: 38- (Some Changes)

---
+++ Elementary/38- setInterval() Global Function/project/js/index++.js
@@ -0,0 +1,11 @@
+const following = document.querySelector("#following");
+
+const MAX = 2_065;
+let i = 0;
+const counter = setInterval(() => {
+  if (i > MAX) clearInterval(counter);
+  else {
+    following.innerHTML = i;
+    i++;
+  }
+}, 0.1);
--- Elementary/38- setInterval() Global Function/project/js/index.js
+++ Elementary/38- setInterval() Global Function/project/js/index.js
@@ -1,18 +1,17 @@
 let minute = +prompt("Enter minute: ");
 let second = +prompt("Enter second: ");
-second++;
+
+const useLeadingZero = (num) => num.toString().padStart(2, "0");
 
 const timer = setInterval(() => {
-  if (second === 0) {
-    if (minute !== 0) {
-      second = 59;
-      minute--;
-    }
-  } else {
-    second--;
+  if (second < 0) {
+    minute--;
+    second = 59;
   }
   if (minute === 0 && second === 0) {
     clearInterval(timer);
   }
-  console.log(`${minute} : ${second}`);
-}, 1000);
+
+  console.log(`${useLeadingZero(minute)} : ${useLeadingZero(second)}`);
+  second--;
+}, 1_000);
