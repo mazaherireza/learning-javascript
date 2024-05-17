65f13f3ba1b6adee6173ee9f38f52162c108cf11
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-17 10:21:56 +0330

    Intermediate: 2- Edition

--- Intermediate/02- DOM Access Methods/project/js/index.js
+++ Intermediate/02- DOM Access Methods/project/js/index.js
@@ -1,18 +1,25 @@
-const main = document.getElementById("main");
+const $ = document;
 
-const listItems = document.getElementsByClassName("list-item");
+const main = $.getElementsByTagName("main");
 
+const listItems = $.getElementsByClassName("list-item");
+console.log(`listItems are: ${listItems}`); // listItems are: [object HTMLCollection]
+
+console.log(main);
 console.log(listItems);
 console.log(listItems[0]);
 
-const section = document.getElementsByTagName("section");
+const section = $.getElementsByTagName("section");
 console.log("section is: ", section);
 
-const firstLiElement = document.querySelector(".list-item");
+const firstLiElement = $.querySelector(".list-item");
+console.log(`firstLiElement is: ${firstLiElement}`); // firstLiElement is: [object HTMLLIElement]
 console.log("firstLiElement is: ", firstLiElement);
 
-const liElements = document.querySelectorAll(".list-item");
+const liElements = $.querySelectorAll(".list-item");
+console.log(`liElements are: ${liElements}`); // liElements are: [object NodeList]
 console.log("liElements are: ", liElements);
 
-const playground = document.querySelector("#playground");
+const playground = $.querySelector("#playground");
+console.log(`playground is: ${playground}`); // playground is: [object HTMLElement]
 console.log("playground is: ", playground);
