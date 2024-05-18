7878a6c6c78daa7c72a4c132cd9ccd2639c18495
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-18 12:57:59 +0330

    Intermediate: 12- Edition

--- Intermediate/12- Exercise (KeyboardEvent 2)/project/js/index.js
+++ Intermediate/12- Exercise (KeyboardEvent 2)/project/js/index.js
@@ -1,5 +1,7 @@
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
+const $ = document;
+
+const usernameInput = $.querySelector("#username");
+const passwordInput = $.querySelector("#password");
 
 const isValidUsername = (username) => {
   return username.length >= 10 ? true : false;
@@ -9,43 +11,77 @@ const isValidPassword = (password) => {
   return password.length >= 8 ? true : false;
 };
 
-const validate = (event) => {
-  event.preventDefault();
-  let username = usernameInput.value;
-  let password = passwordInput.value;
+let username;
+let password;
 
-  const modal = document.querySelector(".modal");
-  modal.style.visibility = "visible";
-  if (isValidUsername(username) && isValidPassword(password)) {
-    modal.innerHTML = "Welcome";
-    modal.style.background = "linear-gradient(to left, #78ffd6, #a8ff78)";
-  } else {
-    modal.innerHTML = "Invalid!, Try Again.";
-    modal.style.background = "linear-gradient(to left, #7C0A02, #fd5c63)";
-  }
-  setTimeout(() => {
-    modal.style.visibility = "hidden";
-  }, 5000);
+const setInputs = () => {
+  username = usernameInput.value;
+  password = passwordInput.value;
 };
 
-const usernameValidationInfo = document.querySelector("#username-validation");
-const passwordValidationInfo = document.querySelector("#password-validation");
-console.log(usernameValidationInfo, passwordValidationInfo);
+const resetInputs = () => {
+  usernameInput.value = "";
+  passwordInput.value = "";
+};
+
+const GENERAL_STYLE = {
+  textAlign: "center",
+  fontWeight: "bold",
+  fontSize: "small",
+};
+
+const manipulateNode = (node, message = "MESSAGE", style = {}) => {
+  node.innerHTML = message;
+  const augmentedStyle = { ...style, ...GENERAL_STYLE };
+  const keys = Object.keys(augmentedStyle);
+  keys.forEach((key) => {
+    node.style[key] = augmentedStyle[key];
+  });
+};
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  setInputs();
+  const modal = $.querySelector(".modal");
+  modal.style.visibility = "visible";
+  if (isValidUsername(username) && isValidPassword(password)) {
+    manipulateNode(modal, "Welcome", {
+      background: "linear-gradient(to left, #78ffd6, #a8ff78)",
+    });
+  } else {
+    manipulateNode(modal, "Invalid!, Try Again.", {
+      background: "linear-gradient(to left, #7C0A02, #fd5c63)",
+    });
+  }
+
+  setTimeout(() => {
+    modal.style.visibility = "hidden";
+  }, 5_000);
+
+  resetInputs();
+};
+
+const usernameValidationInfo = $.querySelector("#username-validation");
+const passwordValidationInfo = $.querySelector("#password-validation");
+
+const changeVisibility = (node, value = "visible") => {
+  node.style.visibility = value;
+};
+
 const usernameKeyLog = () => {
   const username = usernameInput.value;
   if (username.length < 12) {
-    usernameValidationInfo.style.visibility = "visible";
-    usernameValidationInfo.style.color = "#7C0A02";
+    changeVisibility(usernameValidationInfo);
   } else {
-    usernameValidationInfo.style.visibility = "hidden";
+    changeVisibility(usernameValidationInfo, "hidden");
   }
 };
+
 const passwordKeyLog = () => {
   const password = passwordInput.value;
   if (password.length < 8) {
-    passwordValidationInfo.style.visibility = "visible";
-    passwordValidationInfo.style.color = "#7C0A02";
+    changeVisibility(passwordValidationInfo);
   } else {
-    passwordValidationInfo.style.visibility = "hidden";
+    changeVisibility(passwordValidationInfo, "hidden");
   }
 };
