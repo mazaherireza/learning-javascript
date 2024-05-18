f83d9a4ee197fcf20e3dd90aff355ce660272b23
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-18 10:22:11 +0330

    Intermediate: 10- Edition

--- Intermediate/10- Exercise (Simple Login Form)/project/js/index.js
+++ Intermediate/10- Exercise (Simple Login Form)/project/js/index.js
@@ -1,5 +1,7 @@
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
+const $ = document;
+
+const usernameInput = $.querySelector("#username");
+const passwordInput = $.querySelector("#password");
 
 const isValidUsername = (username) => {
   return username.length >= 10 ? true : false;
@@ -9,21 +11,55 @@ const isValidPassword = (password) => {
   return password.length >= 8 ? true : false;
 };
 
-const validate = (event) => {
-  event.preventDefault();
-  let username = usernameInput.value;
-  let password = passwordInput.value;
+let username;
+let password;
 
-  const modal = document.querySelector(".modal");
+const setInputs = () => {
+  username = usernameInput.value;
+  password = passwordInput.value;
+};
+
+const reset = () => {
+  usernameInput.value = "";
+  passwordInput.value = "";
+};
+
+const GENERAL_STYLE = {
+  textAlign: "center",
+  fontWeight: "bold",
+  fontSize: "small",
+};
+
+const manipulateNode = (node, message = "MESSAGE", style = {}) => {
+  node.innerHTML = message;
+  const augmentedStyle = { ...style, ...GENERAL_STYLE };
+  const keys = Object.keys(augmentedStyle);
+  keys.forEach((key) => {
+    node.style[key] = augmentedStyle[key]; // <-------- *
+  });
+};
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  setInputs();
+  const modal = $.querySelector(".modal");
   modal.style.visibility = "visible";
   if (isValidUsername(username) && isValidPassword(password)) {
-    modal.innerHTML = "Welcome";
-    modal.style.background = "linear-gradient(to left, #78ffd6, #a8ff78)";
+    manipulateNode(modal, "Welcome", {
+      background: "linear-gradient(to left, #78ffd6, #a8ff78)",
+    });
   } else {
-    modal.innerHTML = "Invalid!, Try Again.";
-    modal.style.background = "linear-gradient(to left, #7C0A02, #fd5c63)";
+    manipulateNode(modal, "Invalid!, Try Again.", {
+      background: "linear-gradient(to left, #7C0A02, #fd5c63)",
+      // textAlign: "center",
+      // fontWeight: "bold",
+      // fontSize: "small",
+    });
   }
+
   setTimeout(() => {
     modal.style.visibility = "hidden";
-  }, 5000);
+  }, 5_000);
+
+  reset();
 };
