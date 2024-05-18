0dd6b059b36fa0970c5e00095c99f0677f5fd305
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-18 15:53:16 +0330

    Intermediate: 15- Edition

--- Intermediate/15- Exercise (Random Background)/project/js/index.js
+++ Intermediate/15- Exercise (Random Background)/project/js/index.js
@@ -2,16 +2,16 @@ const $ = document;
 const playground = $.querySelector("#playground");
 
 const colors = [
-  "#230a0d",
-  "#340e14",
-  "#46131a",
-  "#571821",
-  "#682f37",
-  "#895d64",
-  "#ab8c90",
+  "#ffc300",
+  "#ff5733",
+  "#daf7a6",
+  "#2874a6",
+  "#aed6f1",
+  "#1b4f72",
 ];
 const len = colors.length;
 const DELAY = 5_000;
+
 setInterval(() => {
   const index = Math.floor(Math.random() * len);
   playground.style.background = colors[index];
