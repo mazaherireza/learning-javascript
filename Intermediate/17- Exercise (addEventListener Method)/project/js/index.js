ce4fbc492a6f7001e875079e124ae37d0adf4e6c
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-19 10:21:09 +0330

    Intermediate: 17- Edition

--- Intermediate/17- Exercise (addEventListener Method)/project/js/index.js
+++ Intermediate/17- Exercise (addEventListener Method)/project/js/index.js
@@ -1,18 +1,20 @@
-const $ = document;
-
 const response = {
   Iran: ["Tehran", "Isfahan", "Rasht", "Shiraz", "Kerman", "Yazd"],
   France: ["Paris", "Marseille", "Lyon"],
   England: ["London", "Oxford", "Liverpool"],
 };
 
+const $ = document;
 const country = $.querySelector("#country");
 const city = $.querySelector("#city");
 
 country.addEventListener("change", () => {
   const selectedCountry = country.value;
-  if (selectedCountry != -1) {
-    city.innerHTML = "";
+  city.innerHTML = "";
+  if (selectedCountry == -1) {
+    city.setAttribute("disabled", "disabled");
+  } else {
+    city.removeAttribute("disabled");
     const cities = response[selectedCountry];
     cities.forEach((cityItem) => {
       city.innerHTML += `<option value=${cityItem}>${cityItem}</option>`;
