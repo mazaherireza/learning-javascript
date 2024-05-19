076e99ecf59e958aa36c8ebed73dce0bef6f1072
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-19 11:57:01 +0330

    Intermediate: 18 Edition

--- Intermediate/18 - Exercise (Weather Forcast)/project/js/index.js
+++ Intermediate/18 - Exercise (Weather Forcast)/project/js/index.js
@@ -24,19 +24,45 @@ const response = {
 const $ = document;
 
 const searchBtn = $.querySelector("#search-btn");
+const playground = $.querySelector("#playground");
+
+const resetNodes = () => {
+  city.innerHTML = "";
+  weather.innerHTML = "";
+  temp.innerHTML = "";
+  message.innerHTML = "";
+};
 
 searchBtn.addEventListener("click", () => {
   const keySearch = $.querySelector("#key-search").value.toLowerCase();
   const selectedCity = response[keySearch];
+  const city = $.querySelector("#city");
+  const weather = $.querySelector("#weather");
+  const temp = $.querySelector("#temp");
+  const message = $.querySelector("#message");
+
+  resetNodes();
   if (selectedCity) {
-    const city = $.querySelector("#city");
-    const weather = $.querySelector("#weather");
-    const temp = $.querySelector("#temp");
-    city.innerHTML = selectedCity.name;
-    weather.innerHTML = `Weather: ${selectedCity.weather}`;
-    temp.innerHTML = `Temperature: ${selectedCity.temp} C`;
+    const { name, weather: W, temp: T } = selectedCity;
+    city.innerHTML = name;
+    weather.innerHTML = `Weather: ${W}`;
+    temp.innerHTML = `Temperature: ${T} °C`;
   } else {
-    const information = $.querySelector("#information");
-    information.innerHTML = "Not Found!";
+    message.innerHTML = "Not Found!";
   }
 });
+
+// IIFE
+(() => {
+  const colors = [
+    "#ffc300",
+    "#ff5733",
+    "#daf7a6",
+    "#2874a6",
+    "#aed6f1",
+    "#1b4f72",
+  ];
+  const len = colors.length;
+  const index = Math.floor(Math.random() * len);
+  playground.style.background = colors[index];
+})();
