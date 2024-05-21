d5f468494878d8d492c40bfce1b97673771f1ee9
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-21 21:41:02 +0330

    Intermediate: 38- Exercise (Note App)

--- Intermediate/38- Exercise (Note App)/project/js/index.js
+++ Intermediate/38- Exercise (Note App)/project/js/index.js
@@ -3,38 +3,37 @@ const $ = document;
 const note = $.querySelector("#note");
 const colors = $.querySelectorAll(".color");
 const selectedColor = $.querySelector("#selected-color");
+
 const notes = $.querySelector("#notes");
+
 const addBtn = $.querySelector("#add");
 const refreshBtn = $.querySelector("#refresh");
 
-let backgrounColor = "#FFF";
-
-const appendNote = (span) => {
-  notes.appendChild(span);
-};
+let backgroundColor = "#FFF";
 
 const deleteNote = (event) => {
   event.target.parentElement.remove();
 };
 
 const createNote = () => {
-  if (note.value.trim()) {
+  const title = note.value.trim();
+  if (title) {
     const div = $.createElement("div");
     const span = $.createElement("span");
-    const i = $.createElement("i");
+    const icon = $.createElement("i");
 
-    i.className = "fa fa-trash";
-    span.innerHTML = note.value.trim();
+    icon.className = "fa fa-trash";
+    span.innerHTML = title;
 
-    i.addEventListener("click", (event) => {
+    icon.addEventListener("click", (event) => {
       deleteNote(event);
     });
     div.appendChild(span);
-    div.appendChild(i);
+    div.appendChild(icon);
 
-    div.style.background = backgrounColor;
+    div.style.background = backgroundColor;
     div.className = "card";
-    appendNote(div);
+    notes.appendChild(div);
     note.value = "";
   }
 };
@@ -51,13 +50,13 @@ addBtn.addEventListener("click", () => {
 
 refreshBtn.addEventListener("click", () => {
   note.value = "";
-  backgrounColor = "#FFF";
-  selectedColor.style.backgroundColor = backgrounColor;
+  backgroundColor = "#FFF";
+  selectedColor.style.backgroundColor = backgroundColor;
 });
 
 colors.forEach((color) => {
   color.addEventListener("click", () => {
-    backgrounColor = color.style.backgroundColor;
-    selectedColor.style.backgroundColor = backgrounColor;
+    backgroundColor = color.style.backgroundColor;
+    selectedColor.style.backgroundColor = backgroundColor;
   });
 });
