e39e0c2ccb35249450a74ae1d91edcc6189624ae
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-21 22:24:05 +0330

    Intermediate: 39- onscroll Event

--- Intermediate/37- onscroll Event/project/js/index.js
+++
@@ -1,9 +0,0 @@
-const $ = document;
-
-const text = $.querySelector("#text");
-
-text.addEventListener("scroll", (event) => {
-  console.log(event);
-  console.log(text.scrollTop);
-  console.log(text.scrollLeft);
-});
---
+++ Intermediate/39- onscroll Event/project/js/index.js
@@ -0,0 +1,17 @@
+const $ = document;
+
+const text = $.querySelector("#text");
+
+const counter = $.querySelector("#counter");
+let movement = 0;
+
+text.addEventListener("scroll", (event) => {
+  console.log(text.scrollTop);
+  console.log(text.scrollLeft);
+  movement++;
+  counter.innerHTML = movement;
+});
+
+window.addEventListener("load", () => {
+  counter.innerHTML = movement;
+});
