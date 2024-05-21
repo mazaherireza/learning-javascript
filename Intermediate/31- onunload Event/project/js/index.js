7ee28ea1b40c3bd3c3ec6dd4d0f0ba12da84a5ac
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-21 10:23:07 +0330

    Intermediate: 33- onunload Event

--- Intermediate/31- onunload Event/project/js/index.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('unload', () => {
-  console.log('Unloaded!')
-})
-
-// onunloaded occurs when the user navigates away from the page (by clicking on a link, 
-// submitting a form, closing the browser window, etc.)
---
+++ Intermediate/33- onunload Event/project/js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("unload", () => {
+  // Deprecated: 21 May, 2024
+  console.log("Unloaded!");
+});
+
+/* onunloaded occurs when the user navigates away from the page (by clicking on a link, 
+  submitting a form, closing the browser window, etc.)
+*/
