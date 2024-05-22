3acb93557a6be2571b8e37f733a2e129a5b71c71
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-22 13:16:17 +0330

    Intermediate: 42- Exercise (Audio Object)

--- Intermediate/42- Exercise (Audio Object)/project/js/index.js
+++ Intermediate/42- Exercise (Audio Object)/project/js/index.js
@@ -5,14 +5,19 @@ const playBtn = $.querySelector("#play");
 const pauseBtn = $.querySelector("#pause");
 const rateBtn = $.querySelector("#rate");
 
+const icon = $.querySelector(".fa-music");
+
 playBtn.addEventListener("click", () => {
   audio.play();
+  icon.style.visibility = "visible";
 });
 
 pauseBtn.addEventListener("click", () => {
   audio.pause();
+  icon.style.visibility = "hidden";
 });
 
 rateBtn.addEventListener("click", () => {
-  audio.playbackRate = 1.5;
+  const num = +rateBtn.innerHTML.split("x")[0];
+  audio.playbackRate = num;
 });
