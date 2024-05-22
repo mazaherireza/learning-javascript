76248cf7282f5855b98d2b28b9b5eeb0c5d34f0e
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-22 17:13:21 +0330

    Intermediate: 44- Exercise (Music Player)

--- Intermediate/44- Exercise (Music Player)/project/js/index.js
+++ Intermediate/44- Exercise (Music Player)/project/js/index.js
@@ -1,15 +1,17 @@
 const $ = document;
 
-const icons = $.querySelectorAll("i");
-const audioList = $.querySelectorAll("audio");
+const icons = $.querySelectorAll("i.fa");
+const audios = $.querySelectorAll("audio");
+const singerWrapper = $.querySelector(".singer-wrapper");
 const singer = $.querySelector("#singer");
 
 let key;
-icons.forEach((mediaCard) => {
-  mediaCard.addEventListener("click", (event) => {
+icons.forEach((icon) => {
+  icon.addEventListener("click", (event) => {
     key = event.target.dataset.singer;
-    audioList.forEach((audio) => {
+    audios.forEach((audio) => {
       singer.innerHTML = key;
+      singerWrapper.style.visibility = "visible";
       if (audio.dataset.singer == key) {
         audio.currentTime = 0;
         audio.play();
