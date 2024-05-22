07096cfb4ceeef6b1a5ede99676fd51b3f21813d
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-22 11:29:56 +0330

    Intermediate: 41- Exercise (dataset Property)

--- Intermediate/39- dataset Property/project/js/index.js
+++
@@ -1,9 +0,0 @@
-const $ = document;
-
-const buttons = $.querySelectorAll(".btn");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    console.log(event.target.dataset); // DOMStringMap ...
-  });
-});
