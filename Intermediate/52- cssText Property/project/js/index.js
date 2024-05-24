bbafec96bb3f584c6244af153db3c6ce38a4219b
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-24 20:43:30 +0330

    Intermediate: 54- cssText Property

--- Intermediate/52- cssText Property/project/js/index.js
+++
@@ -1,9 +0,0 @@
-const $ = document;
-
-const btn = $.querySelector("button");
-const h6 = $.querySelector("h6");
-
-btn.addEventListener("click", () => {
-  h6.style.cssText =
-    "width: 8em; font-size: 18px; border-radius: 0.5em; background-color: yellow; padding: 1em;";
-});
