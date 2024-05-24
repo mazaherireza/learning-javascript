f83ed98bde04d225c1731ce2ba02b7f58c0c4944
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-24 21:23:25 +0330

    Intermediate: 55- Exercise (Photo Slide)

--- Intermediate/55- Exercise (Photo Slider)/project/js/index.js
+++ Intermediate/55- Exercise (Photo Slider)/project/js/index.js
@@ -1,18 +1,20 @@
 const $ = document;
 
 const img = $.querySelector("#img");
+
 const num = $.querySelector("#num");
 const total = $.querySelector("#total");
+
 const prev = $.querySelector("#prev");
 const next = $.querySelector("#next");
 
 const PATH = "./assets/images/";
 const photos = [
   "Vivian_Maier_Self_Portrait.jpg",
-  "Vivian_Maiter_Street_Photgraphy1.jpg",
-  "Vivian_Maiter_Street_Photgraphy2.jpg",
-  "Vivian_Maiter_Street_Photgraphy3.jpg",
-  "Vivian_Maiter_Street_Photgraphy4.jpg",
+  "Vivian_Maier_Street_Photgraphy1.jpg",
+  "Vivian_Maier_Street_Photgraphy2.jpg",
+  "Vivian_Maier_Street_Photgraphy3.jpg",
+  "Vivian_Maier_Street_Photgraphy4.jpg",
 ];
 
 let selectedIndex = 0;
@@ -26,13 +28,13 @@ const showSlide = () => {
   num.innerHTML = selectedIndex + 1;
 };
 
-showSlide();
-
-prev.addEventListener("click", () => {
+const goToPrev = () => {
   selectedIndex--;
   if (selectedIndex < 0) selectedIndex = TOTAL - 1;
   showSlide();
-});
+};
+
+prev.addEventListener("click", goToPrev);
 
 const goToNext = () => {
   selectedIndex++;
@@ -46,6 +48,10 @@ const interval = setInterval(() => {
   showSlide();
   goToNext();
 }, 2_500);
+
+window.addEventListener("load", () => {
+  showSlide();
+});
 
 window.addEventListener("unload", () => {
   clearInterval(interval);
