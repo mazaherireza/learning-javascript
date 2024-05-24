bd4a7e701b90691b77910e5aed964f6f957dd380
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-24 17:39:38 +0330

    Intermediate: Exercise (Animation Events)

--- Intermediate/50- Exercise (Animation Events)/project/js/index.js
+++ Intermediate/50- Exercise (Animation Events)/project/js/index.js
@@ -3,19 +3,29 @@ const $ = document;
 const btn = $.querySelector("button");
 const img = $.querySelector("img");
 const heading = $.querySelector("h4");
+const message = $.querySelector("h5");
+const count = $.querySelector("#count");
+const time = $.querySelector("#time");
 
 btn.addEventListener("click", () => {
-  img.style.animation = "move 2s 2";
+  img.style.animation = "change 2s 3";
 });
 
 img.addEventListener("animationstart", () => {
   heading.innerHTML = "Animation Starts";
 });
 
-img.addEventListener("animationiteration", () => {
+let counter = 1;
+let elapsedTime;
+img.addEventListener("animationiteration", (event) => {
+  counter++;
   heading.innerHTML = "Animation Iterates";
+  elapsedTime = event.elapsedTime;
 });
 
 img.addEventListener("animationend", () => {
   heading.innerHTML = "Animation Ended";
+  message.style.visibility = "visible";
+  count.innerHTML = counter;
+  time.innerHTML = elapsedTime;
 });
