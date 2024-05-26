41b349b80d6180f92f2db79660ee8fa11204cc90
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-26 21:51:08 +0330

    Intermediate: 62- Exercise (Simple Text Generator)

--- Intermediate/62- Exercise (Simple Text Generator)/project/js/index.js
+++ Intermediate/62- Exercise (Simple Text Generator)/project/js/index.js
@@ -1,8 +1,11 @@
 const $ = document;
 
-const num = $.querySelector("#num");
-const quoteList = $.querySelector("#quote-list");
+const min = $.querySelector("#min");
+const max = $.querySelector("#max");
+
+const input = $.querySelector('input[type="number"]');
 const btn = $.querySelector("#btn");
+const quoteList = $.querySelector("#quote-list");
 
 const quotes = [
   "Travel, because Google can't answer all of your questions about life.",
@@ -10,20 +13,26 @@ const quotes = [
   "LIVE what you LOVE.",
   "Everything is beautiful if it gets me closer to you.",
   "You took the sun with you, when you left.",
+  "Even the darkest night will end and the sun will rise!",
+  "Be loyal to your future not to your past.",
+  "Choose to be optimistic, it feels better.",
+  "The first wealth is health.",
+  "One day you'll laugh at the problems you have today.",
 ];
+
 const LEN = quotes.length;
 
 const MIN = 1;
-const MAX = 5;
+const MAX = 10;
 
 const renderToDOM = () => {
   quoteList.innerHTML = "";
-  const numValue = parseInt(num.value);
+  const numValue = parseInt(input.value);
   if (numValue >= MIN && numValue <= MAX) {
     const slicedQuotes = quotes
       .slice(0, numValue)
       .map((quote) => `<div class="card quote">${quote}</div>`);
-    quoteList.innerHTML = slicedQuotes;
+    quoteList.innerHTML = slicedQuotes.join("");
   } else {
     const p = $.createElement("p");
     p.innerHTML = `Number must be between ${MIN} and ${MAX}`;
@@ -32,12 +41,19 @@ const renderToDOM = () => {
   }
 };
 
-num.addEventListener("click", renderToDOM);
+input.addEventListener("click", renderToDOM);
 
-num.addEventListener("keydown", (event) => {
+input.addEventListener("keydown", (event) => {
   if (event.code == "Enter") renderToDOM();
 });
 
 btn.addEventListener("click", () => {
   renderToDOM();
+});
+
+window.addEventListener("load", () => {
+  min.innerHTML = MIN;
+  max.innerHTML = MAX;
+  input.setAttribute("min", MIN);
+  input.setAttribute("max", MAX);
 });
