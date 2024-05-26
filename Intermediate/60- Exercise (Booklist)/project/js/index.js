1093ae37fe010db00efb9c51ff657f6766d4abc5
mazaherireza <rezamazaheri.email@gmail.com>
2024-05-26 09:08:15 +0330

    Intermediate: 60- Exercise (Booklist)

--- Intermediate/60- Exercise (Booklist)/project/js/index.js
+++ Intermediate/60- Exercise (Booklist)/project/js/index.js
@@ -2,22 +2,24 @@ const $ = document;
 
 const bookTitle = $.querySelector("#book-title");
 const bookAuthor = $.querySelector("#book-author");
+
 const bookTitleError = $.querySelector("#book-title-error");
 const bookAuthorError = $.querySelector("#book-author-error");
-const num = $.querySelector("#num");
-const tbody = $.querySelector("tbody");
 
-const PREFIX = "BK";
-let bookList = [];
+const num = $.querySelector("#num");
+
+const tbody = $.querySelector("tbody");
 
 const addBtn = $.querySelector("#add-btn");
 
+let bookList = [];
+
 const isValid = (value, input = "title") => {
-  let _isValid = true;
+  let validation = true;
   try {
     if (!value.length) {
-      _isValid = false;
-      throw "Invalid Input";
+      validation = false;
+      throw `Invalid ${input}`;
     }
   } catch (error) {
     if (input == "title") {
@@ -26,17 +28,16 @@ const isValid = (value, input = "title") => {
       bookAuthorError.innerHTML = error;
     }
   } finally {
-    return _isValid;
+    return validation;
   }
 };
 
-const createTd = () => $.createElement("td");
 const showNum = () => {
   num.innerHTML = bookList.length;
 };
 
-const deleteRow = (event, { id }) => {
-  const index = bookList.findIndex((book) => book.id === id);
+const deleteRow = (event, { title }) => {
+  const index = bookList.findIndex((book) => book.title === title);
   if (index >= 0) {
     bookList.splice(index, 1);
     localStorage.setItem("bookList", JSON.stringify(bookList));
@@ -49,46 +50,53 @@ const deleteRow = (event, { id }) => {
 
 const representOnDOM = (book) => {
   const tr = $.createElement("tr");
-  const t = createTd();
-  t.innerHTML = book.title;
-  const a = createTd();
-  a.innerHTML = book.author;
-  const action = createTd();
+
+  const title = $.createElement("td");
+  title.innerHTML = book.title;
+
+  const author = $.createElement("td");
+  author.innerHTML = book.author;
+
+  const action = $.createElement("td");
   const btn = $.createElement("button");
   btn.innerHTML = "Delete";
   action.append(btn);
+
   btn.addEventListener("click", (event) => {
     deleteRow(event, book);
   });
-  tr.append(t, a, action);
+
+  tr.append(title, author, action);
   tbody.append(tr);
   showNum();
 };
 
 const doSomeCleanUp = () => {
-  bookTitle.focus();
   bookTitle.value = "";
   bookAuthor.value = "";
+  bookTitle.focus();
 };
 
 addBtn.addEventListener("click", () => {
   const title = bookTitle.value.trim();
   const author = bookAuthor.value.trim();
+
   const titleValidation = isValid(title, "title");
   if (titleValidation) {
     bookTitleError.style.visibility = "hidden";
   } else {
     bookTitleError.style.visibility = "visible";
   }
+
   const authorValidation = isValid(author, "author");
   if (authorValidation) {
     bookAuthorError.style.visibility = "hidden";
   } else {
     bookAuthorError.style.visibility = "visible";
   }
+
   if (titleValidation && authorValidation) {
     const book = {
-      id: `${PREFIX}${bookList.length}`,
       title,
       author,
     };
