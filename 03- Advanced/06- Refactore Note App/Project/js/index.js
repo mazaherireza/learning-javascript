e94026b62e1c10a306c4dafdc43898eb5bb5cc89
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-08 06:57:16 +0330

    Advanced: 06- Refactore Note App

---
+++ 03- Advanced/06- Refactore Note App/Project/js/index.js
@@ -0,0 +1,61 @@
+const $ = document;
+
+const note = $.querySelector("#note");
+const colors = $.querySelectorAll(".color");
+const selectedColor = $.querySelector("#selected-color");
+
+const notes = $.querySelector("#notes");
+
+const addBtn = $.querySelector("#add");
+const refreshBtn = $.querySelector("#refresh");
+
+let backgroundColor = "#FFF";
+
+const createNote = () => {
+  const title = note.value.trim();
+  if (title) {
+    const div = $.createElement("div");
+    const span = $.createElement("span");
+    const icon = $.createElement("i");
+
+    icon.className = "fa fa-trash";
+    span.innerHTML = title;
+
+    div.appendChild(span);
+    div.appendChild(icon);
+
+    div.style.background = backgroundColor;
+    div.className = "card";
+    notes.appendChild(div);
+    note.value = "";
+  }
+};
+
+notes.addEventListener("click", (event) => {
+  if ((event.target.tagName = "I")) {
+    event.target.parentElement.remove();
+  }
+});
+
+note.addEventListener("keyup", (event) => {
+  if (event.code == "Enter") {
+    createNote();
+  }
+});
+
+addBtn.addEventListener("click", () => {
+  createNote();
+});
+
+refreshBtn.addEventListener("click", () => {
+  note.value = "";
+  backgroundColor = "#FFF";
+  selectedColor.style.backgroundColor = backgroundColor;
+});
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    backgroundColor = color.style.backgroundColor;
+    selectedColor.style.backgroundColor = backgroundColor;
+  });
+});
