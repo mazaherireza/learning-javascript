5fe8a85c19db44f7125d0115c6662f13a2a24199
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-09 20:53:12 +0330

    Advanced: 07- Callbacks

---
+++ 03- Advanced/07- Callbacks/project/js/index.js
@@ -0,0 +1,14 @@
+const nums = [1_001, 1_002, 1_003, 1_004, 1_005];
+
+const double = (x) => x * 2;
+
+const iterate = (callback) => {
+  const result = [];
+  nums.forEach((num) => {
+    result.push(callback(num));
+  });
+  return result;
+};
+
+console.log(iterate(double))
+// double is called a callback function, it is passed to iterate as an argument.
