4ed805e49c34dd902e961353f0a1c51c7a6e2cad
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-12 06:29:38 +0330

    Advanced: 18- The Arguments Object

---
+++ 03- Advanced/18- The Arguments Object/project/js/index.js
@@ -0,0 +1,27 @@
+/* 
+  const sum = (first, second) => { <-------- (Arrow Function)
+    console.log(arguments); <--------- Uncaught ReferenceError: arguments is not defined
+  };
+*/
+
+function sum() {
+  /* 
+    If a function is called with too many arguments (more than declared),
+    these arguments can be reached using the "arguments" object. 
+  */
+
+  console.log(arguments);
+  console.log(typeof arguments); // object
+  // arguments.forEach((element) => { <---------- Uncaught TypeError: arguments.forEach is not a function
+  //   console.log(element);
+  // });
+  const len = arguments.length;
+  let sum = 0;
+  for (let index = 0; index < len; index++) {
+    sum += arguments[index];
+  }
+  return sum;
+}
+
+const percentages = [10, 20, 5, 40, 10, 35];
+console.log(sum(...percentages));
