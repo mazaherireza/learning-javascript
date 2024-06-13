9a38ecd926915ba16ce492ead25f4dbfed2a6813
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-13 22:46:20 +0330

    Advanced: 22- for...of Loop

---
+++ 03- Advanced/22- for...of Loop/project/js/index.js
@@ -0,0 +1,19 @@
+// for of statement loops through the values of an iterable object.
+// ... iterable DSs such as Arrays, Strings, Maps, NodeList and more.
+
+const evenNumbers = [100, 102, 104, 106, 108];
+for (let num of evenNumbers) console.log(num);
+
+const fullName = "Reza Mazaheri";
+for (let char of fullName) console.log(char);
+for (let char in fullName) console.log(char); // for in <------------ *
+
+function sum() {
+  let sum = 0;
+  // arguments.forEach() <---------------- X
+  for (arg of arguments) // <-------------- Inside function ...
+    sum += arg;
+  return sum;
+}
+
+console.log(sum(...evenNumbers));
