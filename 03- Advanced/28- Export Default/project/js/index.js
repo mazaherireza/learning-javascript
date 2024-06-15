9561ca8685119c391a4debadf589c80a28e85d86
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-15 12:44:28 +0330

    Advanced: 28- Export Defaults

---
+++ 03- Advanced/28- Export Default/project/js/index.js
@@ -0,0 +1,10 @@
+import { salutation as salut } from "./utils.js"; // <-------- With js extension.
+import something from "./utils.js";
+
+import * as util from "./utils.js";
+console.log("Util is: ", util); // Module, default, salutation
+util.salutation();
+console.log(util.default);
+
+console.log(salut());
+console.log(something);
---
+++ 03- Advanced/28- Export Default/project/js/utils.js
@@ -0,0 +1,5 @@
+const salutation = (firstName = "Reza") => `Hello ${firstName}.`;
+const COUNT = 13;
+
+export { salutation };
+export default COUNT;
