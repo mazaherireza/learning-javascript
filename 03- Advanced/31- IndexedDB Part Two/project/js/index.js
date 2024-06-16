c7fc8aaefd435a58b6e6bbed0747ea5ca48284d0
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-17 00:05:03 +0330

    Advanced: 31- IndexedDB Part Two

---
+++ 03- Advanced/31- IndexedDB Part Two/project/js/index.js
@@ -0,0 +1,45 @@
+import isSupported from "./utils.js";
+
+let DB;
+
+const createDatabase = () => {
+  const NAME = "CodeCademy";
+  let version = 1;
+  const request = indexedDB.open(NAME, version);
+  // Event handling
+  request.onerror = (error) => {
+    console.error("IndexedDB error: ", error);
+  };
+  request.onsuccess = (event) => {
+    console.info("Successful DB connection");
+    DB = request.result;
+  };
+
+  request.onupgradeneeded = (event) => {
+    console.info("DB created.");
+    DB = request.result;
+    DB.onerror = () => console.error("Error, loading DB.");
+    if (!DB.objectStoreNames.contains("Users")) {
+      const OBJECT_STORE = "Users";
+      const objectStore = DB.createObjectStore(OBJECT_STORE, {
+        keyPath: "username",
+      });
+      // Define what data items the objectStore will contain.
+      objectStore.createIndex("username", "username", { unique: true });
+      objectStore.createIndex("password", "password", { unique: false });
+      objectStore.createIndex("email", "email", { unique: false });
+
+      objectStore.transaction.oncomplete = () => {
+        // Transaction completed
+        console.info(`Object Store ${OBJECT_STORE} created.`);
+      };
+    }
+    if (DB.objectStoreNames.contains("Orders")) DB.deleteObjectStore("Orders");
+  };
+};
+
+window.onload = () => {
+  isSupported()
+    ? createDatabase()
+    : console.warn("Your browser doesn't support IndexedDB.");
+};
