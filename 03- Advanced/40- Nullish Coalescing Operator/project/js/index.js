9f2f8dac3c7ced155789b76768f5cdd5b78a49ff
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-19 15:51:35 +0330

    Advanced: 40- Nullish Coalescing Operator

---
+++ 03- Advanced/40- Nullish Coalescing Operator/project/js/index.js
@@ -0,0 +1,25 @@
+/* 
+  Nullish coalescing operator (??)
+  --------------------------------
+  ... null or undefined ... <-------------------- ***
+
+  Relationship with the optional chaining operator (?.)
+  ... which is useful to access a property of an object which may be null or undefined.
+
+  Combining them, you can safely access a property of an object which may be nullish and 
+  provide a default value if it is. <------------ **
+*/
+
+let counter = NaN;
+const index = counter ?? 1;
+console.log(index); // NaN
+
+const user = {
+  username: "rezamazaheri",
+  firstName: "Reza",
+  lastName: "Mazaheri",
+};
+
+console.log(user?.family ?? "Not Available");
+
+// It is not possible to  combine both && and || directly with ??. ... providing ()
