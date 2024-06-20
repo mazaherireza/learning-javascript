a5f6f8c0748e17ce1be8241688a8f3b0f123d13b
mazaherireza <rezamazaheri.email@gmail.com>
2024-06-20 21:44:47 +0330

    Advanced: 41- Optional Chaining

---
+++ 03- Advanced/41- Optional Chaining/project/js/index.js
@@ -0,0 +1,25 @@
+/*
+  Optional chaining (?.)
+  ----------------------
+  The optional chaining (?.) operator accesses an object's property or calls a function.
+  If the object accessed or function called using this operator is undefined or null,
+  ... evaluates to undefined instead of throwing an error.
+
+  JS
+  obj.val?.prop
+  obj.val?.[expr]
+  obj.func?.(args)
+*/
+
+const user = {
+  username: "rezamazaheri",
+  firstName: "Reza",
+  lastName: "Mazaheri",
+};
+
+console.log(user.courses); // undefiend
+console.log(user.courses[0]); // Uncaught TypeError: Cannot read properties of undefined (reading '0')
+console.log(user.courses?.[0]); // undefined
+console.log(user.courses?.[0].title); // undefined
+
+console.log(undefined?.name); // undefined
