de41b01d3eeaae8b374f3225294fca83af19a514
mazaherireza <rezamazaheri.email@gmail.com>
2024-07-02 11:53:00 +0330

    Advanced: 62- JSON

---
+++ 03- Advanced/62- JSON/project/js/index.js
@@ -0,0 +1,20 @@
+/*
+  What is JSON? 
+  -------------
+  JavaScript Object Notation
+  JSON is a lightweight format for storing and transporting data
+  ... is a text format 
+
+  If you parse the JSON string with a JS program, you can access the data as an object.
+
+  JSON is language independent <----------------- **
+*/
+
+/*
+  JSON Syntax Rule
+  ----------------
+  Data is in name/value pairs
+  Data is separated by ,
+  {} hold objects
+  [] hold arrays
+*/
