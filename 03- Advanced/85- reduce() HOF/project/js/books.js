e61915086f013edc4515d888a2ddb87d4f00c5a9
mazaherireza <rezamazaheri.email@gmail.com>
2024-07-05 08:28:39 +0330

    Advanced: 85- reduce() HOF

---
+++ 03- Advanced/85- reduce() HOF/project/js/books.js
@@ -0,0 +1,19 @@
+const books = [
+  {
+    title: "The Death of Ivan Ilyich",
+    author: "Leo Tolstoy",
+    grade: 4.8,
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    grade: 4.6,
+  },
+  {
+    title: "خانه‌ی ادریسی‌ها",
+    author: "غزاله علیزاده",
+    grade: 4.9,
+  },
+];
+
+export { books };
---
+++ 03- Advanced/85- reduce() HOF/project/js/index.js
@@ -0,0 +1,11 @@
+import { books } from "./books.js";
+
+const result = books.reduce((prev, current) => {
+  if (prev.grade >= current.grade) return prev;
+  else return current;
+});
+console.log(result.grade);
+
+const compare = ({ grade: gradeA }, { grade: gradB }) => gradB - gradeA;
+const sorted = books.sort(compare);
+console.log(sorted);
