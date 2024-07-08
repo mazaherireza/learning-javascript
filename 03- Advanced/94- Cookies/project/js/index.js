c49a819c4003c667750a49b886b814e31b4985c4
mazaherireza <rezamazaheri.email@gmail.com>
2024-07-08 12:09:48 +0330

    Advanced: 94- Edition

--- 03- Advanced/94- Cookies/project/js/index.js
+++ 03- Advanced/94- Cookies/project/js/index.js
@@ -2,7 +2,7 @@ import { calculateExpirationDate } from "./utils.js";
 
 const $ = document;
 window.onload = () => {
-  const exday = calculateExpirationDate(10);
-  $.cookie = `username=rezamazaheri;path=/;expires=${exday}`;
+  const exdays = calculateExpirationDate(10);
+  $.cookie = `username=rezamazaheri;path=/;expires=${exdays}`;
   console.log($.cookie); // username=rezamazaheri
 };
