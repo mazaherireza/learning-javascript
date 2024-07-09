af444213132c54d3b47e5edc4fc662dc77a0b4e0
mazaherireza <rezamazaheri.email@gmail.com>
2024-07-09 13:38:00 +0330

    Advanced: 101- Error Handling

---
+++ 03- Advanced/101- Error Handling/project/js/index.js
@@ -0,0 +1,53 @@
+const user = {
+  username: "rezamazaheri",
+  firstName: "Reza",
+  lastName: "Mazaheri",
+};
+
+const INTERVAL = 2_000;
+
+const isLogin = ({ username }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Do Something
+      if (username.startsWith("r")) resolve(true);
+      else reject(false);
+    }, INTERVAL);
+  });
+};
+
+const getCourses = (username) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Do Something
+      console.log(`Username is: ${username}.`);
+      resolve(["Introduction to JavaSciprt", "Introduction to Python"]);
+    }, INTERVAL);
+  });
+};
+
+const getCourseInfo = (title) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Do Something
+      console.log(`Title is: ${title}`);
+      resolve({
+        title,
+        duration: "8h 45m",
+        instructor: "Ramtin Khosravi",
+      });
+    }, INTERVAL);
+  });
+};
+
+async function execute() {
+  try {
+    await isLogin(user);
+    const courses = await getCourses(user.username);
+    const course = await getCourseInfo(courses[0]);
+    console.log(course);
+  } catch (error) {
+    console.error(error);
+  }
+}
+execute();
