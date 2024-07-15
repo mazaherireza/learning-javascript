5dbbc3121adbceaefc87e9e1d27be22a82be7207
mazaherireza <rezamazaheri.email@gmail.com>
2024-07-15 21:05:59 +0330

    npm: 11- The scripts Field

---
+++ 06- npm/11- The scripts Field/code/introduction.js
@@ -0,0 +1,9 @@
+const FULL_NAME = "Reza Mazaheri";
+const thisYear = new Date().getFullYear();
+const yearOfBirth = 1989;
+
+console.log(
+  `Hello World!, I'm ${FULL_NAME}, I'm ${
+    thisYear - yearOfBirth
+  }. An ambitious frontend developer who interested in JavaScript and its modern frameworks.`
+);
