aae32555730b08d93bea15ab8efdc7072225d512
mazaherireza <rezamazaheri.email@gmail.com>
2024-07-22 19:41:44 +0330

    PWA: 03- promise-based Edition

--- 07- PWA/03- The First PWA/promise-based/code/service-worker.js
+++ 07- PWA/03- The First PWA/promise-based/code/service-worker.js
@@ -1,17 +1,19 @@
+const VERSION = "v1";
+const CACHE_NAME = `Learning-PWA-${VERSION}`;
+
 const STATIC_RESOURCES = [
   "/",
   "/fallback.html",
+  "/fallback.css",
   "./assets/images/favicon.png",
   "./style/main.css",
   "./style/index.css",
   "./style/mobile.css",
   "./style/tablet.css",
-  "./manifest.json",
   "./js/index.js",
+  "./manifest.json",
+  "./assets/images/img_512.png",
 ];
-
-const VERSION = "v1";
-const CACHE_NAME = `Learning-PWA-${VERSION}`;
 
 // On install, cache the static resources.
 self.addEventListener("install", (event) => {
@@ -43,18 +45,24 @@ self.addEventListener("activate", (event) => {
 
 // On fetch, intercept server requests and respond with cached responses instead of going to network.
 self.addEventListener("fetch", (event) => {
+  // Skip the request, if request is not made with http protocol.
+  if (!event.request.url.indexOf("http") == 0) return;
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
         return response;
       } else {
         console.log(event.request.url);
-        return fetch(event.request).then((serverResponse) => {
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, serverResponse.clone());
-            return serverResponse;
+        return fetch(event.request)
+          .then((serverResponse) => {
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, serverResponse.clone());
+              return serverResponse;
+            });
+          })
+          .catch(() => {
+            return caches.match("/fallback.html");
           });
-        });
       }
     })
   );
