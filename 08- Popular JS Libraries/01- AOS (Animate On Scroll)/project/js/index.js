cc72349f5fc960bcf27192bfec39fb0c5ee0252a
mazaherireza <rezamazaheri.email@gmail.com>
2024-08-02 10:16:17 +0330

    Popular JS Libraries: 01- Edition

---
+++ 08- Popular JS Libraries/01- AOS (Animate On Scroll)/project/js/index.js
@@ -0,0 +1,23 @@
+const $ = document;
+const container = $.querySelector("#container-content");
+
+const imgs = [
+  "./assets/images/1.png",
+  "./assets/images/2.png",
+  "./assets/images/3.png",
+];
+
+const renderToDOM = () => {
+  imgs.forEach((img) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `<div data-aos="fade-up">
+            <img src=${img} alt="Portrait" />
+        </div>`
+    );
+  });
+};
+
+window.onload = () => {
+  renderToDOM();
+};
