b2a5d67ab924ffbe679c83a1d48901df4a393417
mazaherireza <rezamazaheri.email@gmail.com>
2024-08-16 11:03:50 +0330

    Popular JS Libraries: 11- ClientJS

---
+++ 08- Popular JS Libraries/11- ClientJS/project/js/index.js
@@ -0,0 +1,7 @@
+const client = new ClientJS();
+
+console.log(client.isMac());
+console.log(client.getBrowser());
+console.log(client.getBrowserMajorVersion());
+console.log(client.getLanguage());
+
