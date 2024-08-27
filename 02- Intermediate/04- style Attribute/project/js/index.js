c5eaabc39fb5b4de92b6134afec5b43fd343dc28
mazaherireza <rezamazaheri.email@gmail.com>
2024-08-27 09:39:26 +0330

    Intermediate: 04- Edition

--- 02- Intermediate/04- style Attribute/project/js/index.js
+++ 02- Intermediate/04- style Attribute/project/js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector("#playground-title");
 title.style.border = "1px solid black";
-title.style.padding = "0.5em";
-title.style.borderRadius = "0.25em";
+title.style.padding = "0.5rem";
+title.style.borderRadius = "0.25rem";
 title.style.fontWeight = "bold";
