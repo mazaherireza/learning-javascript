f131fc4cec3f247b52ad4b05545dd07d824df94f
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-19 10:48:53 +0330

    Intermediate: 17 - Edition

--- 02- Intermediate/17- Exercise (addEventListener Method)/project/js/index.js
+++ 02- Intermediate/17- Exercise (addEventListener Method)/project/js/index.js
@@ -1,8 +1,4 @@
-const countries = {
-  Iran: ["Tehran", "Isfahan", "Rasht", "Shiraz", "Kerman", "Yazd"],
-  France: ["Paris", "Marseille", "Lyon"],
-  England: ["London", "Oxford", "Liverpool"],
-};
+import { countries } from "./countries.js";
 
 const $ = document;
 const country = $.querySelector("#country");
