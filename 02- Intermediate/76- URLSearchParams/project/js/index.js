640cf806efb2700f65b039a71f6b6b463ad724dc
mazaherireza <rezamazaheri.email@gmail.com>
2024-09-26 11:26:52 +0330

    Intermediate: 76- Edition

--- 02- Intermediate/76- URLSearchParams/project/js/index.js
+++ 02- Intermediate/76- URLSearchParams/project/js/index.js
@@ -1,6 +1,6 @@
 // The URLSearchParams interface defines utility methods to work with the query string of a URL.
 
-//location = "https://quera.org/problemset?difficulty=EZ&tag=80"
+// location = "https://quera.org/problemset?difficulty=EZ&tag=80"
 
 const searchParams = new URLSearchParams(location.search);
 const tagParameter = searchParams.get("tag"); // 80
