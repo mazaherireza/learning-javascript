89403958170ef04e74acee986e10148cb476d282
mazaherireza <rezamazaheri.email@gmail.com>
2024-11-15 18:52:08 +0330

    Advanced: 13- Edition

--- 03- Advanced/13- Scope/project/js/index.js
+++ 03- Advanced/13- Scope/project/js/index.js
@@ -2,7 +2,7 @@ let x = "Global Scope";
 console.log(`In global scope x is: ${x}`);
 
 const f = () => {
-  //let x = "Local Scope"
+  let x = "Local Scope"
   console.log(`In f function x is: ${x}`);
 };
 
