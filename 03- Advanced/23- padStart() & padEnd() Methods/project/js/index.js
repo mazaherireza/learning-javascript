a75536974a95ed830aad8e3c0a16c9ccfc9e03bc
mazaherireza <rezamazaheri.email@gmail.com>
2024-11-17 08:57:52 +0330

    Advanced: 23- Edition

--- 03- Advanced/23- padStart() & padEnd() Methods/project/js/index.js
+++ 03- Advanced/23- padStart() & padEnd() Methods/project/js/index.js
@@ -1,7 +1,7 @@
 // The padStart() method is a string method.
 // To pad a number, convert the number to a string first.
 
-const mask = (phoneNumber, sign="#") => {
+const mask = (phoneNumber, sign = "#") => {
   const START = 0;
   const END = 4;
   // 0935
@@ -9,9 +9,11 @@ const mask = (phoneNumber, sign="#") => {
 
   const COUNT = 8;
   const FILLING = sign;
+  // 0935****
   const masked = pre.padEnd(COUNT, FILLING);
 
   const OFFSET = 3;
+  // 902
   const last = phoneNumber.slice(-OFFSET);
   return `${masked}${last}`;
 };
