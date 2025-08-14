494ecb726af0b90e71752c93ce2453978a4f7829
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-14 21:43:50 +0330

    Elementary: 08- refactor

--- 01- Elementary/08- String Methods/code/scripts/index.js
+++ 01- Elementary/08- String Methods/code/scripts/index.js
@@ -1,8 +1,8 @@
 const motto = "I Love JavaScript";
 
-console.log(motto.charCodeAt(0)); // ASCII Code
+console.log(motto.charCodeAt(0)); // 73 (ASCII Code)
 
-console.log(motto.concat(" & VueJS"));
+console.log(motto.concat(" & React.js"));
 
 console.log(motto.search("Java")); // 7
 console.log(motto.search("java")); // -1
