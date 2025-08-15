ff362cad21debf6b5c03dbad1cc1937b3fe3b258
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-15 10:22:41 +0330

    Elementary: 23 - refactor

--- 01- Elementary/23- Exercise (Using some Higher Order Function)/code/scripts/index.js
+++ 01- Elementary/23- Exercise (Using some Higher Order Function)/code/scripts/index.js
@@ -1,20 +1,21 @@
 const users = [
   {
-    _id: "AERY_1001",
+    _id: "USR_1001",
     username: "rezamazaheri",
     firstName: "Reza",
     lastName: "Mazaheri",
-    age: 35,
+    age: 36,
   },
   {
-    _id: "AYRY_8008",
+    _id: "USR_8008",
     username: "yo3ef_am",
     firstName: "Yousef",
     lastName: "Amini",
-    age: 25,
+    age: 26,
   },
 ];
 
 const username = "yo3ef_am";
+
 const hasAccount = users.some((user) => user.username == username);
 hasAccount ? console.log("Welcome") : console.log("Sign Up");
