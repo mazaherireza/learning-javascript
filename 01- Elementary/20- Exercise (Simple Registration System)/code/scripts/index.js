542158c43ef5938331a9b8eb34f4bcc254394e86
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-15 09:29:18 +0330

    Elementary: 20 - refactor

--- 01- Elementary/20- Exercise (Simple Registration System)/code/scripts/index.js
+++ 01- Elementary/20- Exercise (Simple Registration System)/code/scripts/index.js
@@ -1,15 +1,16 @@
 const users = [
   {
-    _id: "AERY_1",
+    _id: "USR_1",
     username: "rezamazaheri",
     firstName: "Reza",
     lastName: "Mazaheri",
-    age: 35,
+    age: 36,
   },
 ];
 
 const firstName = prompt("Enter Your First Name: ", "Name");
 const lastName = prompt("Enter Your Last Name: ", "Last Name");
+
 let age = prompt("Enter Your Age: ", 18);
 
 while (isNaN(age) || age.length > 3 || +age <= 0) {
@@ -18,14 +19,16 @@ while (isNaN(age) || age.length > 3 || +age <= 0) {
 }
 const MIN = 3;
 const MAX = 15;
+
 let username = prompt("Enter Your Username: ", "username");
+
 while (username.length < MIN || username.length > MAX) {
   alert(`Username Must Be Between ${MIN} and ${MAX} Characters Long.`);
   username = prompt("Enter Your Username: ", "username");
 }
 
 users.push({
-  _id: `AERY_${users.length + 1}`,
+  _id: `USR_${users.length + 1}`,
   username,
   firstName,
   lastName,
