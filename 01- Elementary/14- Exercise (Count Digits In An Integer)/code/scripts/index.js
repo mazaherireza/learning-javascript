2bbe557f5e9325838e0012fcff1f9ff6c2512f92
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-15 07:37:35 +0330

    Elementary: 14- refactor

--- 01- Elementary/14- Exercise (Count Digits In An Integer)/code/scripts/index.js
+++ 01- Elementary/14- Exercise (Count Digits In An Integer)/code/scripts/index.js
@@ -1,11 +1,13 @@
 let n = parseInt(prompt("Enter number: ", 0)); // parseInt deletes leading zeros. (021 converts to 21)
 let count = 0;
 
-if (n == 0) count = 1;
-else
+if (n == 0) {
+  count = 1;
+} else {
   while (n > 0) {
     count++;
     n = Math.floor(n / 10);
   }
+}
 
-console.log(`Count of digit(s) is: ${count}.`);
+console.log(`Count of digit(s) is: ${count}`);
