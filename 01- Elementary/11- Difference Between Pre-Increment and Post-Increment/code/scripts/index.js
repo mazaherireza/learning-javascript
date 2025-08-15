2f94c2e723b538633136f8e587e3c9f39860c53c
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-15 07:15:59 +0330

    Elementary: 11- refactor

--- 01- Elementary/11- Difference Between Pre-Increment and Post-Increment/code/scripts/index.js
+++ 01- Elementary/11- Difference Between Pre-Increment and Post-Increment/code/scripts/index.js
@@ -9,11 +9,11 @@ print();
 
 x = 0;
 // Post-Increment
-y = x++; // <-------------------------- 1.Assignment 2.Increment
+y = x++; // <-------------------------- 1) Assignment  2) Increment
 
 print(); // x is 1 and y is 0.
 
 x = 0;
 // Pre-Increment
-y = ++x; // <-------------------------- 1.Increment 2.Assignment
+y = ++x; // <-------------------------- 1) Increment  2) Assignment
 print(); // x is 1 and y is 1
