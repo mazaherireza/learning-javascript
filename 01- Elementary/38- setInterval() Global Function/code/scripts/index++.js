befa94368e00d9c7f66fbe820d0cdbb95ecc9a49
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-16 14:33:59 +0330

    Elementary: 38, 40- refactor

--- 01- Elementary/38- setInterval() Global Function/code/scripts/index++.js
+++ 01- Elementary/38- setInterval() Global Function/code/scripts/index++.js
@@ -5,8 +5,9 @@ const MAX = 2_065;
 let i = 0;
 
 const counter = setInterval(() => {
-  if (i > MAX) clearInterval(counter);
-  else {
+  if (i > MAX) {
+    clearInterval(counter);
+  } else {
     following.innerHTML = i;
     i++;
   }
