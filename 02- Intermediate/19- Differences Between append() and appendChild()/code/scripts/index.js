ed608d55c78607ee67f42f08d684a4ff44ff898c
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-25 06:29:23 +0330

    Intermedaite: 19- refactor

--- 02- Intermediate/19- Differences Between append() and appendChild()/code/scripts/index.js
+++ 02- Intermediate/19- Differences Between append() and appendChild()/code/scripts/index.js
@@ -6,13 +6,15 @@ const btn = $.querySelector("#add-btn");
 btn.addEventListener("click", () => {
   const input = $.querySelector("#input");
   const item = input.value.trim();
+
   const li = $.createElement("li");
   li.innerHTML = item;
-  // input: node
-  // output: HTMLElement
+  /* input: node
+  output: HTMLElement */
   unorderedList.appendChild(li);
-  // input: strings or nodes
-  // output: void
-  // unorderedList.append()
+  
+  /* input: strings or nodes
+  output: void
+  unorderedList.append() */
   input.value = "";
 });
