547c27dea4bc876620469a77b287036a2c3f194b
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-28 14:29:47 +0330

    Intermediate: 39- refactor

--- 02- Intermediate/39- onscroll Event/code/scripts/index.js
+++ 02- Intermediate/39- onscroll Event/code/scripts/index.js
@@ -3,9 +3,10 @@ const $ = document;
 const text = $.querySelector("#text");
 
 const counter = $.querySelector("#counter");
+
 let movement = 0;
 
-text.addEventListener("scroll", (event) => {
+text.addEventListener("scroll", (_) => {
   console.log(text.scrollTop);
   console.log(text.scrollLeft);
   movement++;
