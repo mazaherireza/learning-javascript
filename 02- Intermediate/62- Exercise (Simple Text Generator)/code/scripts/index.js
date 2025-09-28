3d24a6bb8933bfd3d4bf65ed990ed018779a81c5
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-28 11:34:55 +0330

    Intermediate: 62 - Refactor

--- 02- Intermediate/62- Exercise (Simple Text Generator)/code/scripts/index.js
+++ 02- Intermediate/62- Exercise (Simple Text Generator)/code/scripts/index.js
@@ -6,46 +6,54 @@ const min = $.querySelector("#min");
 const max = $.querySelector("#max");
 
 const input = $.querySelector('input[type="number"]');
-const btn = $.querySelector("#btn");
-const quoteList = $.querySelector("#quote-list");
+const button = $.querySelector("button");
+const quotes = $.querySelector("#quote-list");
 
 const MIN = 1;
 const MAX = 10;
 
 const renderToDOM = () => {
-  quoteList.innerHTML = "";
+  quotes.innerHTML = "";
+
   const numValue = parseInt(input.value);
+
   if (numValue >= MIN && numValue <= MAX) {
     const slicedQuotes = quotes
       .slice(0, numValue)
       .map((quote) => `<div class="card quote">${quote}</div>`);
-    quoteList.innerHTML = slicedQuotes.join("");
+      
+    quotes.innerHTML = slicedQuotes.join("");
   } else {
     const p = $.createElement("p");
+
     p.innerHTML = `Number must be between ${MIN} and ${MAX}`;
+
     p.className = "error card";
-    quoteList.append(p);
+
+    quotes.append(p);
   }
 };
 
-// input.addEventListener("click", renderToDOM);
-
 input.addEventListener("keydown", (event) => {
   const { code } = event;
-  if (code === "Enter") renderToDOM();
+
+  if (code === "Enter") {
+    renderToDOM();
+  }
 });
 
 input.addEventListener("change", () => {
   renderToDOM();
 });
 
-btn.addEventListener("click", () => {
+button.addEventListener("click", () => {
   renderToDOM();
 });
 
 window.addEventListener("load", () => {
   min.innerHTML = MIN;
   max.innerHTML = MAX;
+
   input.setAttribute("min", MIN);
   input.setAttribute("max", MAX);
 });
