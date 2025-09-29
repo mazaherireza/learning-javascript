e81faab5e2cccdb041d3a8d90e6d3725c42c2c5d
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-29 18:23:52 +0330

    Intermediate: 73 - Refactor

--- 02- Intermediate/73- Window Screen Object/code/scripts/index.js
+++ 02- Intermediate/73- Window Screen Object/code/scripts/index.js
@@ -1,14 +1,19 @@
 console.log(screen);
 
-console.log(screen.width); // returns the width of the visitor's screen in pixels.
-console.log(screen.availWidth);
+// returns the width of the visitor's screen in pixels.
+console.log(screen.width);
+
 // returns the width of the visitor's screen, in pixels, minus interface features like the Windows Taskbar.
+console.log(screen.availWidth);
 
 console.log(screen.height);
 console.log(screen.availHeight);
 
-console.log(screen.colorDepth); // ... read-only property returns the number of bits used to display one color.
-console.log(screen.pixelDepth); // returns the pixel depth of the screen.
+// ... read-only property returns the number of bits used to display one color.
+console.log(screen.colorDepth);
+
+// returns the pixel depth of the screen.
+console.log(screen.pixelDepth);
 
 // If there is not adequate bit depth choose a simpler color.
 /*
