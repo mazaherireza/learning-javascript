656b87a28463184b6819123b252ceb1a3773f0c0
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-30 09:06:07 +0330

    Advanced: 03- Refactor

--- 03- Advanced/03- Event Bubbling/code/scripts/index.js
+++ 03- Advanced/03- Event Bubbling/code/scripts/index.js
@@ -4,6 +4,7 @@ const divisions = $.querySelectorAll(".division");
 divisions.forEach((division) => {
   division.addEventListener("click", (event) => {
     console.log(event.target);
+    
     console.log(division.getAttribute("class").split(" ")[1]);
   });
 });
