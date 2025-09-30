e04ccee10aa91c55842e8d0cc5b0119f4427f5ad
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-30 09:33:22 +0330

    Advanced: 05- Refactor

--- 03- Advanced/05- stopPropagation Method/code/scripts/index.js
+++ 03- Advanced/05- stopPropagation Method/code/scripts/index.js
@@ -4,7 +4,9 @@ const divisions = $.querySelectorAll(".division");
 divisions.forEach((division) => {
   division.addEventListener("click", (event) => {
     event.stopPropagation();
+
     console.log(event.target);
+
     console.log(division.getAttribute("class").split(" ")[1]);
   });
 });
