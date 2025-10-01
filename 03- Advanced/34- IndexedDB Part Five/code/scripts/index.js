e59bd64ec5ac83b7d4da1a2fedeedb07c56dc3c3
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-01 16:33:55 +0330

    Advanced: 34- Refactor

--- 03- Advanced/34- IndexedDB Part Five/code/scripts/index.js
+++ 03- Advanced/34- IndexedDB Part Five/code/scripts/index.js
@@ -6,18 +6,20 @@ import {
   resetFields,
 } from "./formElements.js";
 
-// Save the connection to the DB in a global variable since it will later be used to carry out transactions.
+// Save the connection to the DB, in a global variable since it will later be used to carry out transactions.
 let DB; // IndexedDB connection
 const STORE_NAME = "Users";
 
 const createDatabase = () => {
   const NAME = "Learning";
   const VERSION = 1;
+
   const request = indexedDB.open(NAME, VERSION);
 
   request.onsuccess = (event) => {
     // Create DB connection
     DB = request.result;
+
     getUsers();
   };
 
@@ -27,11 +29,14 @@ const createDatabase = () => {
 
   request.onupgradeneeded = (event) => {
     DB = request.result;
+
     DB.onerror = () => console.error("Error, loading DB.");
+
     if (!DB.objectStoreNames.contains(STORE_NAME)) {
       const objectStore = DB.createObjectStore(STORE_NAME, {
         keyPath: "username",
       });
+
       // Define what data items the objectStore will contain.
       objectStore.createIndex("username", "username", { unique: true });
       objectStore.createIndex("password", "password", { unique: false });
@@ -41,24 +46,31 @@ const createDatabase = () => {
         console.info("Object store 'Users' created.");
       };
     }
-    if (DB.objectStoreNames.contains("Orders")) DB.deleteObjectStore("Orders");
+
+    if (DB.objectStoreNames.contains("Orders")) {
+      DB.deleteObjectStore("Orders");
+    }
   };
 };
 
 const addUser = (user) => {
   const transaction = DB.transaction(STORE_NAME, "readwrite");
+
   transaction.onerror = (error) => {
     console.warn(error);
   };
+
   transaction.onsuccess = (event) => {
     console.info(event);
   };
 
   const store = transaction.objectStore(STORE_NAME);
   const request = store.add(user);
+
   request.onerror = (error) => {
     console.warn(`Error to add new user: ${error}`);
   };
+
   request.onsuccess = (event) => {
     console.info(event);
     console.info(`New user added, username: ${request.result}`);
@@ -78,52 +90,67 @@ const populateTable = (users) => {
       `
     )
     .join("");
+
   tbody.innerHTML = userTemplate;
 };
 
 const getUsers = () => {
   const transaction = DB.transaction(STORE_NAME, "readonly");
+
   transaction.onerror = (error) => {
     console.warn(error);
   };
+
   transaction.oncomplete = (event) => {
     console.info(event);
   };
 
   const store = transaction.objectStore(STORE_NAME);
   const request = store.getAll();
+
   request.onerror = (error) => {
     console.warn(`Error to get Users: ${error}`);
   };
+
   request.onsuccess = (event) => {
     console.info(event);
     console.info("Got all the users.");
+
     const users = request.result;
+
     console.table(users);
+
     populateTable(users);
   };
 };
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
   addUser({
     username: username.value,
     password: password.value,
     email: email.value,
   });
+
   resetFields();
+
   username.focus();
+
   getUsers();
 };
 
 const removeUser = (key) => {
   const transaction = DB.transaction(STORE_NAME, "readwrite");
+
   transaction.onerror = (error) => {
     console.warn(error);
   };
+
   transaction.oncomplete = (event) => {
     console.info(event);
   };
+
   const store = transaction.objectStore(STORE_NAME);
   const request = store.delete(key);
 
@@ -134,20 +161,25 @@ const removeUser = (key) => {
   request.onsuccess = (event) => {
     console.info(event);
     console.log(`User deleted, username: ${request.result}`);
+
     getUsers();
   };
 };
 
 tbody.onclick = (event) => {
   const target = event.target;
+
   if (target.tagName == "I") {
     const grandParent = target.parentNode.parentNode;
     const key = grandParent.querySelector("td").innerHTML;
+
     const confirmation = confirm(
       `Are your sure that you want to delete this row with username ${key}`
     );
+
     if (confirmation) {
       target.remove();
+
       removeUser(key);
     }
   }
