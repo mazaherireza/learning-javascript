f6204ba2d99d4e7ba3ff45daab66dcc4db5e899d
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-01 15:08:21 +0330

    Advanced: 32- Refactor

--- 03- Advanced/32- IndexedDB Part Three/code/scripts/index.js
+++ 03- Advanced/32- IndexedDB Part Three/code/scripts/index.js
@@ -6,31 +6,39 @@ import {
   resetFields,
 } from "./formElements.js";
 
-// Save the connection to the DB in a global variable since it will later be used to carry out transactions.
+// Save the connection to the DB, in a global variable since it will later be used to carry out transactions.
 let DB; // IndexedDB connection
 
 const createDatabase = () => {
   const NAME = "Learning";
   const VERSION = 1;
+  
   const request = indexedDB.open(NAME, VERSION);
   // Event handling
   request.onerror = (error) => {
     console.error("IndexedDB error: ", error);
   };
+
   request.onsuccess = (event) => {
     console.info("Successful DB connection");
+
     DB = request.result;
   };
 
   request.onupgradeneeded = (event) => {
     DB = request.result;
+
     console.log(DB);
+
     DB.onerror = () => console.error("Error, loading DB.");
+
     const OBJECT_STORE = "Users";
+
     if (!DB.objectStoreNames.contains(OBJECT_STORE)) {
       const objectStore = DB.createObjectStore(OBJECT_STORE, {
         keyPath: "username",
       });
+
       // Define what data items the objectStore will contain.
       objectStore.createIndex("username", "username", { unique: true });
       objectStore.createIndex("password", "password", { unique: false });
@@ -40,24 +48,31 @@ const createDatabase = () => {
         console.info(`Object store ${OBJECT_STORE} created.`);
       };
     }
-    if (DB.objectStoreNames.contains("Orders")) DB.deleteObjectStore("Orders");
+
+    if (DB.objectStoreNames.contains("Orders")) {
+      DB.deleteObjectStore("Orders");
+    }
   };
 };
 
 const addUser = (user) => {
   const transaction = DB.transaction("Users", "readwrite");
+
   transaction.onerror = (error) => {
     console.warn(error);
   };
+
   transaction.onsuccess = (event) => {
     console.info(event);
   };
 
   const store = transaction.objectStore("Users");
   const request = store.add(user);
+
   request.onerror = (error) => {
     console.warn(`Error to add new user: ${error}`);
   };
+
   request.onsuccess = (event) => {
     console.info(event);
     console.info(`New user added, username: ${request.result}`);
@@ -66,12 +81,15 @@ const addUser = (user) => {
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
   addUser({
     username: username.value,
     password: password.value,
     email: email.value,
   });
+
   resetFields();
+
   username.focus();
 };
 
