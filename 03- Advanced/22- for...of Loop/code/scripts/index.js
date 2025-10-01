5e5c9bcc2b5209bf2e0f8e528c62a2f36330aa34
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-01 11:38:16 +0330

    Advanced: 22- Refactor

--- 03- Advanced/22- for...of Loop/code/scripts/index.js
+++ 03- Advanced/22- for...of Loop/code/scripts/index.js
@@ -2,17 +2,30 @@
 // ... iterable DSs such as Arrays, Strings, Maps, NodeList and more.
 
 const evenNumbers = [100, 102, 104, 106, 108];
-for (const num of evenNumbers) console.log(num);
+
+for (const num of evenNumbers) {
+  console.log(num);
+}
 
 const fullName = "Reza Mazaheri";
-for (const char of fullName) console.log(char);
-for (const char in fullName) console.log(char); // for in <------------ *
+
+for (const char of fullName) {
+  console.log(char);
+}
+
+for (const char in fullName) {
+  console.log(char); // for in <------------ *
+}
 
 function sum() {
   let sum = 0;
+
   // arguments.forEach() <---------------- X
-  for (const arg of arguments) // <-------------- Inside function ...
+
+  for (const arg of arguments) {
     sum += arg;
+  }
+
   return sum;
 }
 
