d4e506a6e61c8e11551a5f5eda8fb7f9b6b05df1
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-01 11:16:49 +0330

    Advanced: 19- Refactor

--- 03- Advanced/19- Function Rest Parameter/code/scripts/index.js
+++ 03- Advanced/19- Function Rest Parameter/code/scripts/index.js
@@ -3,9 +3,12 @@
 
 const sum = (...restParameter) => {
   let sum = 0;
+
   restParameter.forEach((num) => (sum += num));
+
   return sum;
 };
 
 const percentages = [10, 5, 10, 25, 10];
+
 console.log(sum(...percentages));
