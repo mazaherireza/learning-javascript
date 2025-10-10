75b669fbbe3dc822ff36b91692aa3ac430ae505d
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-10 20:09:34 +0330

    Advanced: 84, 85- Refactor

--- 03- Advanced/85- reduce() HOF/code/scripts/index.js
+++ 03- Advanced/85- reduce() HOF/code/scripts/index.js
@@ -1,9 +1,13 @@
 import { books } from "./books.js";
 
 const result = books.reduce((prev, current) => {
-  if (prev.grade >= current.grade) return prev;
-  else return current;
+  if (prev.grade >= current.grade) {
+    return prev;
+  } else {
+    return current;
+  }
 });
+
 console.log(result.grade);
 
 const compare = ({ grade: gradeA }, { grade: gradeB }) => gradeB - gradeA;
