43d6b997aa0f7742172fb9fb904660229310f8cb
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-10 19:08:35 +0330

    Advanced: 81- Refactor

--- 03- Advanced/81- Battery Status API/code/scripts/index.js
+++ 03- Advanced/81- Battery Status API/code/scripts/index.js
@@ -6,11 +6,16 @@ window.onload = () => {
   let batteryIsCharging = false;
   navigator.getBattery().then((battery) => {
     batteryIsCharging = battery.charging;
-    if (batteryIsCharging) console.log("Battery is charging");
+
+    if (batteryIsCharging) {
+      console.log("Battery is charging");
+    }
+
     battery.onlevelchange = (event) => {
       // is fired when the battery level property is updated.
       console.log(event);
     };
+
     battery.onchargingchange = (event) => {
       // is fired when the battery charging property is updated.
       console.log(event);
