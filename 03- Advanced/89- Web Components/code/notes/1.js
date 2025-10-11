03fac1f44f462aeb19eb4ae851aebdf8397d7284
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-11 17:03:05 +0330

    Advanced: 89- Edit

--- 03- Advanced/89- Web Components/code/notes/1.js
+++ 03- Advanced/89- Web Components/code/notes/1.js
@@ -6,6 +6,7 @@
   a custom element's behavior or layout by running some page JavaScript or CSS.
 
   Custom elements are implemented as a class which extends either the base HTMLElement or a built-in HTML element ...
+  
   Typically, the custom element itself is a shadow host, ... <---------- **
 */
 
@@ -15,6 +16,7 @@ customElements.define(
     constructor() {
       super();
     }
+
     connectedCallback() {
       // Create a shadow root, the custom element itself is the shadow host.
       const shadowRoot = this.attachShadow({ mode: "open" });
