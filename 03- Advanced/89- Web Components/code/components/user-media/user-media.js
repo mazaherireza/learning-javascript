102346bb8b1fb045901de4b4901328623889cbf2
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-11 16:47:16 +0330

    Advanced: 89- Refactor

--- 03- Advanced/89- Web Components/code/components/user-media/user-media.js
+++ 03- Advanced/89- Web Components/code/components/user-media/user-media.js
@@ -1,23 +1,26 @@
 const $ = document;
+
 const template = $.createElement("template");
 
 template.innerHTML = `
-  <link rel="stylesheet" href="components/user-media/user-media.css" />
+  <link rel="stylesheet" href="/components/user-media/user-media.css" />
   <div class="user-container">
     <div>
       <h5>Reza Mazaheri</h5>
       <h6>Frontend Developer</h6>
     </div>
-    <img src="components/user-media/assets/Reza_Mazaheri.png" alt="Reza Mazaheri" />
+    <img src="/components/user-media/assets/reza-mazaheri.png" alt="" />
   </div>
 `;
 
 class UserMedia extends HTMLElement {
   constructor() {
     super();
+
     const shadowRoot = this.attachShadow({
       mode: "open",
     });
+
     shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
