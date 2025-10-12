034fc640bf0aeacb5ac1d19e06c26f24c9365828
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-12 16:05:47 +0330

    Advanced: 96- Refactor

--- 03- Advanced/96- Cookies/code/scripts/index.js
+++ 03- Advanced/96- Cookies/code/scripts/index.js
@@ -1,24 +1,31 @@
 import { calculateExpirationDate } from "./utils.js";
 
 const $ = document;
+
 const deleteCookie = (key) => {
   $.cookie = `${key}=; path=/; expires=${calculateExpirationDate(1, "Past")}`;
 };
 
 window.onload = () => {
   const pairs = ["username=rezamazaheri", "fullname=Reza Mazaheri"];
+
   pairs.forEach((nameValue) => {
     $.cookie = `${nameValue}; path=/; expires=${calculateExpirationDate(
       2,
       "Future"
     )}`;
   });
+
   const splittedCookies = $.cookie.split("; ");
+
   const KEY = "fullname";
+
   const wantedPair = splittedCookies.find((cookie) => {
     return cookie.startsWith(`${KEY}=`);
   });
+
   const [key, value] = wantedPair.split("=");
+  
   console.log(value);
 };
 
