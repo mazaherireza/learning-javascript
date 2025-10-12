fd174c9895db00f2d23474f5ae23e436d09c42f8
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-12 15:59:53 +0330

    Advanced: 95- Refactor

--- 03- Advanced/95- Cookies/code/scripts/utils.js
+++ 03- Advanced/95- Cookies/code/scripts/utils.js
@@ -1,7 +1,9 @@
 const calculateExpirationDate = (days = 100) => {
   const date = new Date();
-  // days * 24h * 60min * 60S * 1_000
-  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1_000);
+
+  // days * 24h * 60min * 60S * 1000
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+
   return date;
 };
 
