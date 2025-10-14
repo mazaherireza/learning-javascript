ad79303717248331eca62763cdfc0869596f6041
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-14 18:16:25 +0330

    RE: 03- Refactor

--- 04- Regular Expressions (RegExp)/03- Emoji RegExp/code/scripts/index.js
+++ 04- Regular Expressions (RegExp)/03- Emoji RegExp/code/scripts/index.js
@@ -1,13 +1,15 @@
 import { emojis } from "./emojis.js";
 
 const $ = document;
+
 const textarea = $.querySelector("textarea");
 
 const regex = /:\)/g;
 
 textarea.onkeyup = () => {
   emojis.keys().forEach((characters) => {
-    if (textarea.value.includes(characters))
+    if (textarea.value.includes(characters)) {
       textarea.value = textarea.value.replace(regex, emojis.get(characters));
+    }
   });
 };
