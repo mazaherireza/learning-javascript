512fcf46666c08d64a0ce4943d872440981dd08a
mazaherireza <rezamazaheri.email@gmail.com>
2025-10-14 18:23:49 +0330

    RE: 04- Refactor

--- 04- Regular Expressions (RegExp)/04- Tags in Instagram/code/scritps/index.js
+++ 04- Regular Expressions (RegExp)/04- Tags in Instagram/code/scritps/index.js
@@ -1,4 +1,5 @@
 const $ = document;
+
 const form = $.querySelector("form");
 const textarea = $.querySelector("textarea");
 const paragraph = $.querySelector("form p");
@@ -7,8 +8,10 @@ const regex = /@([\w\.])+/g;
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
   paragraph.innerHTML = textarea.value.replace(regex, (matched) => {
     const reference = matched.slice(1);
+    
     return `<a href="${reference}">${matched}</a>`;
   });
 };
